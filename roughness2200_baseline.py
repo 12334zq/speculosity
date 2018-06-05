f849b65ac9246c4244f2d36e30fbf6a42e80e000
Thibault Wildi <thibault.wildi@epfl.ch>
2018-06-06 00:57:05 +0200

    Improved PCA and Baseline scritps
    Updated names to match the update of the scripts.

--- roughness2200_baseline.py
+++ roughness2200_baseline.py
@@ -1,18 +1,26 @@
-from tensorflow.python.keras.layers import Dense, Conv2D, MaxPool2D, Flatten,\
-    Dropout
+from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras import models
 from tensorflow.python.keras import backend as K
 from datasets.utilities import imagenames, to_onehot
 from imageio import imread
 from sklearn.neighbors import NearestCentroid
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics.pairwise import pairwise_distances
 import numpy as np
 import matplotlib.pyplot as plt
+from progressbar import progressbar
+
+# %% Centred Patches Dataset (Run block before loading)
+root = "./../data~/datasets/roughness2200/centred_patches_64p/"
+img_shape = (64, 64)
+img_shape_flat = (64 * 64,)
+
+# %% 64p Dataset
+root = "./../data~/datasets/roughness2200/64p/"
+img_shape = (64, 80)
+img_shape_flat = (64 * 80,)
 
 # %% Creating Dataset
-root = "./../data~/datasets/roughness2200/64p/"
 classes = ['A2', 'A3', 'A4', 'A5', 'A6', 'A7',
            'B2', 'B3', 'B4', 'B5', 'B6', 'B7']
 classes_Ra = np.array([0.05, 0.1, 0.2, 0.4, 0.8, 1.6])
@@ -20,24 +28,21 @@ classes_Ra = np.array([0.05, 0.1, 0.2, 0.4, 0.8, 1.6])
 # B_classes = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7']
 img_per_class = 2200
 img_names = list(imagenames(img_per_class))
-img_shape = (64, 80)
-img_shape_flat = (64 * 80,)
 nb_classes = 12
 nb_channels = 1
 test_ratio = 0.1
 
-print("Loading data... ", end='\r')
+print("Loading data... ", flush=True)
 
 # Loading Images
-data = []
-for class_name in classes:
-    for img in img_names:
+data = np.empty((img_per_class*nb_classes, *img_shape), dtype=np.float)
+for i in progressbar(range(nb_classes)):
+    class_name = classes[i]
+    for j, img in enumerate(img_names):
         imgpath = root + class_name + '/' + img
-        data.append(imread(imgpath))
+        data[i*img_per_class + j, ...] = imread(imgpath)
 
-data = np.asarray(data)
-data = data.reshape((-1, *img_shape_flat))
-data_full = data.astype(float)
+data_full = data.reshape((-1, *img_shape_flat))
 data_A, data_B = np.split(data_full, 2)
 data_classes = np.split(data_full, nb_classes)
 del data
@@ -104,14 +109,14 @@ print("Pairwise distance: mean = {0:3f}, std = {1:3f}".
       format(np.mean(dist), np.std(dist)))
 
 # %% Nearest Centroid Classifier
-print("Traing Centroid Classifier... ", end='\r')
+print("Traing Centroid Classifier...", flush=True)
 
 model = NearestCentroid()
 acc_full = []
 acc_merged = []
 acc_process = []
 nb_runs = 10
-for i in range(nb_runs):
+for i in progressbar(range(nb_runs)):
     # Randomizing dataset order
     np.random.seed(i)
     indexes = np.arange(data_full.shape[0])
@@ -152,7 +157,6 @@ acc_cross_ab = model.score(data_B, labels_half)
 model.fit(data_B, labels_half)
 acc_cross_ba = model.score(data_A, labels_half)
 
-print("[DONE]")
 print("Accuracy (full labels) : mean = {0:3f}, std = {1:3f} ({2:d} runs)".
       format(np.mean(acc_full), np.std(acc_full), nb_runs))
 print("Accuracy (roughness labels) : mean = {0:3f}, std = {1:3f} ({2:d} runs)".
@@ -165,7 +169,7 @@ print("Accuracy (trained on B, tested on A) : mean = {0:3f}, std = {1:3f} "
       "({2:d} run)".format(acc_cross_ba, 0, 1))
 
 # %% Linear Classifier
-print("Training Linear Classifier... ", end='\r')
+print("Training Linear Classifier... ", flush=True)
 
 
 def new_model(nb_classes):
@@ -182,7 +186,7 @@ acc_full = []
 acc_merged = []
 acc_process = []
 nb_runs = 10
-for i in range(nb_runs):
+for i in progressbar(range(nb_runs)):
     # Randomizing dataset order
     np.random.seed(i)
     indexes = np.arange(data_full.shape[0])
@@ -260,7 +264,6 @@ model.fit(x=data_B,
           verbose=0)
 acc_cross_ba = model.evaluate(x=data_A, y=labels_half_onehot, verbose=0)[1]
 
-print("[DONE]")
 print("Accuracy (full set) : mean = {0:3f}, std = {1:3f} ({2:d} runs)".
       format(np.mean(acc_full), np.std(acc_full), nb_runs))
 print("Accuracy (merged set) : mean = {0:3f}, std = {1:3f} ({2:d} runs)".
@@ -273,7 +276,7 @@ print("Accuracy (trained on B, tested on A) : mean = {0:3f}, std = {1:3f} "
       "({2:d} run)".format(acc_cross_ba, 0, 1))
 
 # %% Linear Regression
-print("Training Linear Regression... ", end='\r')
+print("Training Linear Regression... \t", end='\r')
 
 # Creating regression labels
 reg_labels = classes_Ra[labels_merged.astype(int)]
--- roughness2200_pca.py
+++ roughness2200_pca.py
@@ -1,20 +1,23 @@
-from tensorflow.python.keras.layers import Dense, Conv2D, MaxPool2D, Flatten,\
-    Dropout
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras import models
-from tensorflow.python.keras import backend as K
 from datasets.utilities import imagenames, to_onehot
 from imageio import imread
-from sklearn.neighbors import NearestCentroid
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from sklearn.decomposition import PCA
+from progressbar import progressbar
 
-# %% Creating Dataset
+# %% Centred Patches Dataset (Run block before loading)
+root = "./../data~/datasets/roughness2200/centred_patches_64p/"
+img_shape = (64, 64)
+img_shape_flat = (64 * 64,)
+
+# %% 64p Dataset
 root = "./../data~/datasets/roughness2200/64p/"
+img_shape = (64, 80)
+img_shape_flat = (64 * 80,)
+
+# %% Load Dataset
 classes = ['A2', 'A3', 'A4', 'A5', 'A6', 'A7',
            'B2', 'B3', 'B4', 'B5', 'B6', 'B7']
 classes_Ra = np.array([0.05, 0.1, 0.2, 0.4, 0.8, 1.6])
@@ -22,24 +25,21 @@ classes_Ra = np.array([0.05, 0.1, 0.2, 0.4, 0.8, 1.6])
 # B_classes = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7']
 img_per_class = 2200
 img_names = list(imagenames(img_per_class))
-img_shape = (64, 80)
-img_shape_flat = (64 * 80,)
 nb_classes = 12
 nb_channels = 1
 test_ratio = 0.1
 
-print("Loading data... ", end='\r')
+print("Loading data... ", flush=True)
 
 # Loading Images
-data = []
-for class_name in classes:
-    for img in img_names:
+data = np.empty((img_per_class*nb_classes, *img_shape), dtype=np.float)
+for i in progressbar(range(nb_classes)):
+    class_name = classes[i]
+    for j, img in enumerate(img_names):
         imgpath = root + class_name + '/' + img
-        data.append(imread(imgpath))
+        data[i*img_per_class + j, ...] = imread(imgpath)
 
-data = np.asarray(data)
-data = data.reshape((-1, *img_shape_flat))
-data_full = data.astype(float)
+data_full = data.reshape((-1, *img_shape_flat))
 data_A, data_B = np.split(data_full, 2)
 data_classes = np.split(data_full, nb_classes)
 del data
@@ -60,30 +60,27 @@ labels_merged_onehot = to_onehot(labels_merged, nb_classes)
 # Creating Data-Scaler
 scaler = StandardScaler()
 
-print("[DONE]")
-
 # %% PCA
 
-print("Fitting PCA... ", end='\r')
+print("Fitting PCA... \t", end='\r')
 
 # Normalizing sets
-# data_full_norm = scaler.fit_transform(data_full)
+data_full_norm = scaler.fit_transform(data_full)
 
-pca = PCA(n_components=50)
+pca = PCA(n_components=10)
 pca_full = pca.fit(data_full_norm).transform(data_full_norm)
 
 print("[DONE]")
 
-# %% Plotting
+# %% Plotting Scatter Plot Matrix
 
-
-plt.figure()
+plt.figure('Scatter Plot Matrix of PCA Projections')
 plt.tight_layout()
-N = 4           # Number of PCA components to plot
+N = 4               # Number of PCA components to plot
 colors = ['#8b0000', '#ff0000', '#ff5a00', '#ff9a00', '#ffce00', '#f0ff00',
           '#66d4ff', '#00b8ff', '#009bd6', '#00719c', '#00415a', '#001f2b']
 
-print("Plotting PCA results... ", end='\r')
+print("Plotting PCA results... \t", end='\r')
 
 for row in range(N):
     for col in range(N):
@@ -91,7 +88,7 @@ for row in range(N):
         if row == col:
             for i, class_name, color in zip(range(nb_classes), classes, colors):
                 plt.hist(pca_full[labels_full == i, col],
-                         bins = 20,
+                         bins=20,
                          color=color)
                 plt.xticks([])
                 plt.yticks([])
@@ -109,39 +106,34 @@ for row in range(N):
                 plt.xticks([])
                 plt.yticks([])
 
-plt.subplot(N,N,1)
-plt.ylabel('PCA 1')
-plt.subplot(N,N,5)
-plt.ylabel('PCA 2')
-plt.subplot(N,N,9)
-plt.ylabel('PCA 3')
-plt.subplot(N,N,13)
-plt.xlabel('PCA 1')
-plt.ylabel('PCA 4')
-plt.subplot(N,N,14)
-plt.xlabel('PCA 2')
-plt.subplot(N,N,15)
-plt.xlabel('PCA 3')
-plt.subplot(N,N,16)
-plt.xlabel('PCA 4')
+# Ploting y labels
+for n in range(N):
+    plt.subplot(N, N, 1 + N*n)
+    plt.ylabel('PCA {0:d}'.format(n+1))
 
-# Explained variance
-plt.figure()
+# Ploting y labels
+for n in range(N):
+    plt.subplot(N, N, N*N-n)
+    plt.xlabel('PCA {0:d}'.format(N-n))
+
+print("[DONE]")
+
+
+# %% PLotting PCA Explaind Variance
+
+plt.figure('Explained Variance')
 plt.plot(pca.explained_variance_ratio_*100)
 plt.ylabel('Explained variance [%]')
 plt.xlabel('PCA component')
 plt.grid('on')
-
-
-
-print("[DONE]")
 
 print("The {0:d} first PCA components contain {1:2.2f}%".
       format(N, sum(100*pca.explained_variance_ratio_[:N])))
 
 # %% Plotting PCA Eigen Vectors
 
-print("Plotting PCA Eigen Vectors... ", end='\r')
+print("Plotting PCA Eigen Vectors... \t", end='\r', flush=True)
+
 
 class MidpointNormalize(mpl.colors.Normalize):
     def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
@@ -154,11 +146,12 @@ class MidpointNormalize(mpl.colors.Normalize):
         x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
         return np.ma.masked_array(np.interp(value, x, y))
 
+
 # Eigen vectors
 for n in range(N):
-    eigVec = pca.components_[n,:]
+    eigVec = pca.components_[n, :]
     eigVec = np.reshape(eigVec, img_shape)
-    plt.figure()
+    plt.figure('PCA Eigenvector {0:d}'.format(n+1))
     imgplot = plt.imshow(eigVec,
                          norm=MidpointNormalize(midpoint=0.),
                          cmap='bwr')
@@ -166,6 +159,6 @@ for n in range(N):
     plt.yticks([])
     plt.tight_layout()
     # plt.colorbar()
-    # plt.title("PCA Eigenvector {0:d}".format(n))
-    
+    # plt.title("PCA Eigenvector {0:d}".format(n+1))
+
 print("[DONE]")
